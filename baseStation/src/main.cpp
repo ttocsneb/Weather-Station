0460725623ed0d6f4fb5d39de509e79b5e6f7814
Benjamin Jacobs <contact@ttocsneb.com>
2018-04-10 18:03:36 -0600

    Add module wiringPi

--- baseStation/src/main.cpp
+++ baseStation/src/main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 
-#include "radio.h"
+
 #include "main.h"
+#include "radio.h"
+
+#include <wiringPi.h>
 
 using std::cout;
 using std::endl;
 
 int main(int argc, char** argv) {
+    wiringPiSetup();
+
+    pinMode(27, OUTPUT);
 
     radio::begin();
+
+    for(;;) {
+	cout << "LIGHT ON" << endl;
+	digitalWrite(27, HIGH); delay(500);
+	cout << "LIGHT OFF" << endl;
+	digitalWrite(27, LOW); delay(500);
+    }
 
     return 0;
 }
